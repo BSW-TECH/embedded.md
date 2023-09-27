2547eb69f03c9ad98ca5667e787f3d963b1e847d
Victor S <victor.sofiischi@bsw-tech.com>
2023-09-27 09:55:59 +0300

    Added maintananceCube and configure next config for deploy on ApacheHost

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -1,17 +1,12 @@
 import React from "react"
-import Image from "next/image"
-import pic from '/public/assets/img/work_in_progress.jpg'
+import MaintenanceCube from "@/components/maintanance/MaintananceCube"
 
 const Companies = () => {
   return (
     <>
-      <div className="flex justify-center mt-52">
-        <Image
-          src={pic}
-          alt="Work in progress"
-        >
-
-        </Image>
+      <div className="flex flex-col justify-center items-center overflow-hidden">
+        <span className="text-white text-lg sm:text-4xl pt-6 font-bold font-mono">Work in Progress</span>
+        <MaintenanceCube />
       </div>
     </>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,20 @@ import React from "react"
 import Hero from "@/components/Hero"
 import Card from "@/components/Card"
 import Image from "next/image"
+import back from "../public/hero-bg.png"
 
 const Home = () => {
   return (
     <div className="relative w-full flex flex-center flex-col bg-gray-950">
       <div className="absolute inset-0">
-      <Image
-      src="/assets/img/hero-bg.png"
-      layout="fill"
-      sizes="100vw"
-      objectFit="cover"
-      alt="Cosmos">
+        <Image
+          src={back}
+          layout="fill"
+          sizes="100vw"
+          objectFit="cover"
+          alt="Cosmos">
 
-      </Image>
+        </Image>
       </div>
 
       <div className="flex flex-col items-center relative flex-wrap">
